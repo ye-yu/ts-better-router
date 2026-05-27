37484c48f40fb59f2ebf1d41a561b2e2efe1aca9
raflie <30603031+ye-yu@users.noreply.github.com>
2026-05-27 11:00:39 +0000

    Add dependencies and update router functionality
    - Added "after" and "diff" as dependencies in package.json.
    - Updated test script to import loader.ts.
    - Introduced Router.js.patch to modify Router behavior.
    - Enhanced router methods to support dynamic method assignment.
    - Improved error handling in middleware functions.

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,9 @@
-import type { IncomingMessage, ServerResponse } from 'node:http'
+import { type IncomingMessage, type ServerResponse, METHODS } from 'node:http'
 import { PrefixedLogger } from './logger.ts'
 import { ExtendedMap, PatternMap } from './pattern-map.ts'
 import { WrappedError } from './wrapped-error.ts'
 
+export const methods = METHODS.map(m => m.toLowerCase() as Methods)
 const console = new PrefixedLogger(import.meta.url)
 
 type Middleware = (req: any, res: any, next: (err?: any) => void) => void
@@ -10,7 +11,7 @@ type ErrorMiddleware = (err: any, req: any, res: any, next: (err?: any) => void)
 
 export type Methods = 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head'
 type middlewareRegisterer<TMiddleware extends Function = Middleware, EMiddleware extends Function = ErrorMiddleware, R = RouterBase<TMiddleware, EMiddleware>> = {
-  (path: string, ...middlewares: (TMiddleware | EMiddleware | R)[]): void
+  (path: string, ...middlewares: (TMiddleware | EMiddleware | R)[]): R
 }
 
 type ServerRequest = IncomingMessage & {
@@ -26,13 +27,47 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
 > {
 
   rankingCounter: number;
-  readonly ranks: WeakMap<object, number>
-  readonly trackedPaths: ['use' | Methods, string, (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]][]
+  ranks: WeakMap<object, number>
+  trackedPaths: ['use' | Methods, string, (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]][]
 
   constructor() {
     this.rankingCounter = 0;
     this.ranks = new WeakMap()
     this.trackedPaths = []
+    this.configureUnsetMethods()
+  }
+
+  configureUnsetMethods() {
+    for (const method of METHODS) {
+      if (!this[method as Methods]) {
+        this[method as Methods] = (path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]) => {
+          return this.apply(method as Methods, path, ...middlewares)
+        }
+      }
+    }
+  }
+
+  route: (path: string) => { [K in Methods]: (...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]) => ReturnType<typeof this['route']> } = (path: string) => {
+    const routeHandlers = {} as any
+
+    for (const method of methods) {
+      routeHandlers[method] = (...middlewares: any[]) => {
+        this.apply(method, path, ...middlewares)
+        return routeHandlers
+      }
+    }
+
+    routeHandlers['use' as any] = (...middlewares: any[]) => {
+      (this.use as any)(...middlewares)
+      return routeHandlers
+    }
+
+    routeHandlers['all' as any] = (...middlewares: any[]) => {
+      (this.all as any)(path, ...middlewares)
+      return routeHandlers
+    }
+
+    return routeHandlers
   }
 
   getNewRanking() {
@@ -135,7 +170,24 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
   errorMiddlewaresOfUse = new PatternMap<EMiddleware[]>()
   methodsToErrorMiddlewares = new ExtendedMap<Methods, PatternMap<EMiddleware[]>>()
 
-  use(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
+  all(...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this
+  all(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this
+  all(path: string | (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>), ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    if (typeof path === 'string') {
+      for (const method of methods) {
+        this.apply(method, path, ...middlewares)
+      }
+      return this
+
+    }
+
+    for (const method of methods) {
+      this.apply(method, '/', path, ...middlewares)
+    }
+    return this
+  }
+
+  use(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
     for (const m of middlewares) {
       this.setRanking(m)
       this.trackedPaths.push(['use', path, middlewares])
@@ -150,20 +202,23 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
           this.extractRouterHandlers(m)
           break
         default:
-          throw new Error('Unsupported handler', { cause: m })
+          throw new Error('Unsupported handler: argument handler must be a function', { cause: m })
       }
     }
+
+    return this
   }
 
-  apply(method: Methods, path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]) {
+  apply(method: Methods, path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
     for (const m of middlewares) {
       this.setRanking(m)
       this.trackedPaths.push([method, path, middlewares])
       switch (true) {
+        case typeof m === 'function':
         case this.isMiddleware(m):
           this.methodsToMiddlewares
             .getOrInsertComputed(method, () => new PatternMap())
-            .getOrInsertComputed(path, () => []).push(m)
+            .getOrInsertComputed(path, () => []).push(m as any)
           break
         case this.isErrorMiddleware(m):
           this.methodsToErrorMiddlewares
@@ -174,9 +229,11 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
           this.extractRouterHandlers(m)
           break
         default:
-          throw new Error('Unsupported handler', { cause: m })
+          throw new Error('Unsupported handler: argument handler must be a function', { cause: m })
       }
     }
+
+    return this
   }
 
   extractRouterHandlers(router: RouterBase<any, any>) {
@@ -191,32 +248,32 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
     }
   }
 
-  get(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('get', path, ...middlewares)
+  get(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('get', path, ...middlewares)
   }
 
-  post(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('post', path, ...middlewares)
+  post(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('post', path, ...middlewares)
   }
 
-  put(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('put', path, ...middlewares)
+  put(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('put', path, ...middlewares)
   }
 
-  delete(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('delete', path, ...middlewares)
+  delete(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('delete', path, ...middlewares)
   }
 
-  patch(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('patch', path, ...middlewares)
+  patch(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('patch', path, ...middlewares)
   }
 
-  options(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('options', path, ...middlewares)
+  options(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('options', path, ...middlewares)
   }
 
-  head(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): void {
-    this.apply('head', path, ...middlewares)
+  head(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    return this.apply('head', path, ...middlewares)
   }
 
   extractParams(path: string, pattern: string): Record<string, string> {
@@ -264,7 +321,7 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
   >(req: TRequest, res: TResponse): Promise<void> {
     const { method = '', url = '' } = req
 
-    const methodLower = method.toLowerCase() as unknown as Methods
+    const methodLower = method as unknown as Methods
 
     const asUrlObject = URL.parse(url, 'http://dummy/')
     if (!asUrlObject) {
@@ -360,15 +417,19 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
       }
     }
 
-    if (res.writableEnded) return
-    if (error) {
-      console.debug('Error occurred but writes have not ended, sending 500 response', error)
-      res.statusCode = 500
-      res.end('Internal Server Error')
-    } else {
-      res.statusCode = 404
-      res.end('Not Found')
-    }
+    // if (res.writableEnded) return
+    // if (error) {
+    //   console.debug('Error occurred but writes have not ended, sending 500 response', error)
+    //   res.statusCode = 500
+    //   res.end('Internal Server Error')
+    // } else {
+    //   res.statusCode = 404
+    //   res.end('Not Found')
+    // }
+  }
+
+  handle<TRequest extends { method?: string; url?: string } = IncomingMessage, TResponse extends { statusCode: number; end: (resp?: any) => void; writableEnded: boolean } = ServerResponse>(req: TRequest, res: TResponse): void {
+    this.handleAsync(req, res)
   }
 }
 
@@ -382,16 +443,29 @@ export type RouterType<
 
 
 // @ts-expect-error allow compat with Router() and new Router()
-export const Router: RouterType = function Router() {
-  // @ts-expect-error allow compat with Router() and new Router()
-  if (this.target) {
+export const Router: RouterType = function Router(this: any) {
+  if (!(this instanceof Router)) {
     // @ts-expect-error allow compat with Router() and new Router()
-    const self = this
-
-    Object.setPrototypeOf(self, RouterBase.prototype)
-    return self
+    return new Router()
   }
 
+  function handler(req: any, res: any, next: (err?: any) => void) {
+    // @ts-expect-error allow compat with Router() and new Router()
+    return this.handleAsync(req, res)
+  }
+
+  Object.setPrototypeOf(handler, Router.prototype)
+
   // @ts-expect-error allow compat with Router() and new Router()
-  return new Router()
+  handler.configureUnsetMethods()
+  const base = new RouterBase()
+  for (const [key, value] of Object.entries(base)) {
+    if (Object.hasOwn(handler, key)) continue
+    // @ts-expect-error allow compat with Router() and new Router()
+    handler[key] = value
+  }
+
+  return handler
 }
+
+Object.setPrototypeOf(Router.prototype, RouterBase.prototype);
--- tests/loader.ts
+++ tests/loader.ts
@@ -1,13 +1,24 @@
+import fs from "fs"
+import path from "path"
 import * as module from "module"
 import { cachedFetch } from "./cached-fetch.ts";
+import * as diff from "diff"
+import { fileURLToPath } from "url";
+import { Router, methods } from "../src/router.ts";
 
 const resp = await cachedFetch()("https://raw.githubusercontent.com/expressjs/express/refs/heads/master/test/Router.js")
 const expressRouterTest = resp.ok ? await resp.text() : null
 const expressRouterTestToken = "express-router-test"
 
+const currentDir = fileURLToPath(import.meta.url)
+const patchDir = path.join(path.dirname(currentDir), "..", "patches")
+const patchPath = path.join(patchDir, "Router.js.patch")
+const expressRouterTestPatch = fs.readFileSync(patchPath, "utf-8")
+
+Object.assign(globalThis, { Router, methods })
+
 module.registerHooks({
     resolve(specifier, context, next) {
-        console.log("Loading:", specifier)
         if (specifier === expressRouterTestToken) {
             if (!expressRouterTest) {
                 throw new Error("Failed to fetch express router test")
@@ -28,12 +39,19 @@ module.registerHooks({
             throw new Error("Failed to fetch express router test")
 
         }
+
+        const patched = diff.applyPatch(expressRouterTest, expressRouterTestPatch)
+        if (patched === false) {
+            throw new Error("Failed to apply patch to express router test", { cause: expressRouterTestPatch })
+        }
         return {
             format: "commonjs",
             source: [
-                "var { describe, it }  = require('node:test')",
-                expressRouterTest
+                "var { describe, it: _it }  = require('node:test');",
+                "it = (name, fn) => _it(name, function (done) { this.timeout = () => void 0; Reflect.apply(fn, this, [done]) });",
+                patched,
             ].join('\n'),
+            shortCircuit: true,
         }
     }
 })
