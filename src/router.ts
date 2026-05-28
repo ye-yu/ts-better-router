51d4f76637eb7c0eb3954cb7985a89d5c0a33553
raflie <30603031+ye-yu@users.noreply.github.com>
2026-05-28 04:30:07 +0000

    wip

--- src/router.ts
+++ src/router.ts
@@ -8,9 +8,10 @@ const console = new PrefixedLogger(import.meta.url)
 
 type Middleware = (req: any, res: any, next: (err?: any) => void) => void
 type ErrorMiddleware = (err: any, req: any, res: any, next: (err?: any) => void) => void
+type TFunction = (...args: any[]) => any
 
 export type Methods = 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head'
-type middlewareRegisterer<TMiddleware extends Function = Middleware, EMiddleware extends Function = ErrorMiddleware, R = RouterBase<TMiddleware, EMiddleware>> = {
+type middlewareRegisterer<TMiddleware extends TFunction = Middleware, EMiddleware extends TFunction = ErrorMiddleware, R = RouterBase<TMiddleware, EMiddleware>> = {
   (path: string, ...middlewares: (TMiddleware | EMiddleware | R)[]): R
 }
 
@@ -20,8 +21,9 @@ type ServerRequest = IncomingMessage & {
 }
 
 const emptyMap = new PatternMap<any>()
+type Plugin = "path-to-regexp"
 
-export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware extends Function = ErrorMiddleware> implements Record<
+export class RouterBase<TMiddleware extends TFunction = Middleware, EMiddleware extends TFunction = ErrorMiddleware> implements Record<
   Methods,
   middlewareRegisterer<TMiddleware, EMiddleware, RouterBase<TMiddleware, EMiddleware>>
 > {
@@ -29,16 +31,35 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
   rankingCounter: number;
   ranks: WeakMap<object, number>
   trackedPaths: ['use' | Methods, string, (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]][]
+  shouldCheckResponseEnded: boolean
 
   constructor() {
     this.rankingCounter = 0;
     this.ranks = new WeakMap()
     this.trackedPaths = []
+    this.shouldCheckResponseEnded = false
     this.configureUnsetMethods()
   }
 
+
+  ensureEnded(ensure = true) {
+    this.shouldCheckResponseEnded = ensure
+  }
+
+  plugin(plugin: Plugin, options?: any) {
+    switch(plugin) {
+      case "path-to-regexp": {
+        break
+      }
+      default:
+        throw new Error(`Unsupported plugin: ${plugin}`, { cause: { plugin, options } })
+    }
+
+    return this
+  }
+
   configureUnsetMethods() {
-    for (const method of METHODS) {
+    for (const method of methods) {
       if (!this[method as Methods]) {
         this[method as Methods] = (path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]) => {
           return this.apply(method as Methods, path, ...middlewares)
@@ -188,9 +209,11 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
   }
 
   use(path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    if (!middlewares.length) {
+      throw new Error('Missing handler: argument handler must be a function')
+    }
+
     for (const m of middlewares) {
-      this.setRanking(m)
-      this.trackedPaths.push(['use', path, middlewares])
       switch (true) {
         case this.isMiddleware(m):
           this.middlewaresOfUse.getOrInsertComputed(path, () => []).push(m)
@@ -204,15 +227,19 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
         default:
           throw new Error('Unsupported handler: argument handler must be a function', { cause: m })
       }
+      this.setRanking(m)
+      this.trackedPaths.push(['use', path, middlewares])
     }
 
     return this
   }
 
   apply(method: Methods, path: string, ...middlewares: (TMiddleware | EMiddleware | RouterBase<TMiddleware, EMiddleware>)[]): this {
+    if (!middlewares.length) {
+      throw new Error('Missing handler: argument handler must be a function')
+    }
+
     for (const m of middlewares) {
-      this.setRanking(m)
-      this.trackedPaths.push([method, path, middlewares])
       switch (true) {
         case typeof m === 'function':
         case this.isMiddleware(m):
@@ -231,6 +258,8 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
         default:
           throw new Error('Unsupported handler: argument handler must be a function', { cause: m })
       }
+      this.setRanking(m)
+      this.trackedPaths.push([method, path, middlewares])
     }
 
     return this
@@ -244,6 +273,13 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
           break
         default:
           this.apply(method, path, ...middlewares)
+      }
+    }
+
+    for (const entries of router.paramHandlers.entries()) {
+      const [param, handlers] = entries
+      for (const { handler } of handlers) {
+        this.param(param, handler, param)
       }
     }
   }
@@ -276,7 +312,33 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
     return this.apply('head', path, ...middlewares)
   }
 
-  extractParams(path: string, pattern: string): Record<string, string> {
+  paramHandlers = new Map<string, { handler: (req: any, res: any, next: (err?: any) => void) => void }[]>()
+
+  param(param: string, handler: (req: any, res: any, next: (err?: any) => void) => void, value: string): this {
+    this.setRanking(handler)
+    const handlers = this.paramHandlers.get(param) || []
+    handlers.push({ handler })
+    this.paramHandlers.set(param, handlers)
+    return this
+  }
+
+  // TODO: should refactor to use generic server?
+  *getParamHandlers<
+    TRequest = Parameters<TMiddleware>[0],
+    TResponse = Parameters<TMiddleware>[1]
+  >(param: string): Generator<((req: TRequest, res: TResponse, next: (err?: any) => void) => void)> {
+    const handlers = this.paramHandlers.get(param) || []
+    yield* handlers.toSorted((a, b) => {
+      const aRanking = this.ranks.get(a.handler) || 0
+      const bRanking = this.ranks.get(b.handler) || 0
+      return aRanking - bRanking
+    }).map(e => e.handler)
+  }
+
+  async extractParams<
+    TRequest = Parameters<TMiddleware>[0],
+    TResponse = Parameters<TMiddleware>[1]
+  >(path: string, pattern: string, req: TRequest, res: TResponse): Promise<Record<string, string>> {
     const pathSegments = path.split('/')
     const patternSegments = pattern.split('/')
 
@@ -285,6 +347,11 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
       const patternSegment = patternSegments[i]
       if (patternSegment.startsWith(':')) {
         const paramName = patternSegment.slice(1)
+        for (const paramHandler of this.getParamHandlers(paramName)) {
+          const { resolve, reject, promise } = Promise.withResolvers<void>()
+          paramHandler(req, res, (err) => err ? reject(err) : resolve())
+          await promise
+        }
         params[paramName] = pathSegments[i]
       }
     }
@@ -349,15 +416,19 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
 
     let error: any = null
 
-    const handlers = this.getHandlers(methodLower, url)
+    const handlers = this.getHandlers(methodLower, path)
     handlerLoop: for (const { pattern, handler } of handlers) {
-      const paramsFromPattern = this.extractParams(path, pattern)
+      const paramsFromPattern = await this.extractParams(path, pattern, enrichedReq, res)
       Object.assign(params, paramsFromPattern)
       for (const _middleware of handler) {
         const middleware = _middleware as TMiddleware
         try {
           // note: only resolve when next is called, even if the middleware is a promise
           await new Promise<void>((resolve, reject) => {
+            if (middleware.length !== 3) {
+              Promise.resolve(middleware(enrichedReq, res)).then(resolve).catch(reject)
+              return;
+            }
             const maybePromise = middleware(enrichedReq, res as unknown as any, (err: any) => {
               if (err) {
                 reject(err)
@@ -365,7 +436,6 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
                 resolve()
               }
             })
-
             if (maybePromise instanceof Promise) {
               maybePromise
                 // .then(resolve) // do we need to resolve?
@@ -388,7 +458,7 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
     if (error) {
       const errorHandlers = this.getErrorHandlers(methodLower, url)
       for (const { pattern, handler } of errorHandlers) {
-        const paramsFromPattern = this.extractParams(path, pattern)
+        const paramsFromPattern = await this.extractParams(path, pattern, enrichedReq, res)
         Object.assign(params, paramsFromPattern)
         for (const _middleware of handler) {
           const middleware = _middleware as EMiddleware
@@ -417,15 +487,136 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
       }
     }
 
-    // if (res.writableEnded) return
-    // if (error) {
-    //   console.debug('Error occurred but writes have not ended, sending 500 response', error)
-    //   res.statusCode = 500
-    //   res.end('Internal Server Error')
-    // } else {
-    //   res.statusCode = 404
-    //   res.end('Not Found')
-    // }
+    if (!this.ensureEnded) return
+
+    if (res.writableEnded) return
+    if (error) {
+      console.debug('Error occurred but writes have not ended, sending 500 response', error)
+      res.statusCode = 500
+      res.end('Internal Server Error')
+    } else {
+      res.statusCode = 404
+      res.end('Not Found')
+    }
+  }
+
+  handleMaybeAsync<
+    TRequest extends { method?: string; url?: string } = IncomingMessage,
+    TResponse extends { statusCode: number; end: (resp?: any) => void; writableEnded: boolean } =
+    ServerResponse,
+  >(req: TRequest, res: TResponse): void | Promise<void> {
+    const { method = '', url = '' } = req
+
+    const methodLower = method as unknown as Methods
+
+    const asUrlObject = URL.parse(url, 'http://dummy/')
+    if (!asUrlObject) {
+      res.statusCode = 400
+      res.end('Bad Request')
+      return
+    }
+
+    const path = asUrlObject.pathname || '/'
+    const params: Record<string, string> = {}
+    const query = Object.freeze(Object.fromEntries(asUrlObject.searchParams.entries()))
+    Object.defineProperty(req, 'params', {
+      value: params,
+      writable: false,
+      configurable: false,
+      enumerable: true,
+    })
+    Object.defineProperty(req, 'query', {
+      value: query,
+      writable: false,
+      configurable: false,
+      enumerable: true,
+    })
+    const enrichedReq = req as unknown as ServerRequest
+
+    let error: any = null
+
+    const handlers = this.getHandlers(methodLower, path)
+    handlerLoop: for (const { pattern, handler } of handlers) {
+      const paramsFromPattern = await this.extractParams(path, pattern, enrichedReq, res)
+      Object.assign(params, paramsFromPattern)
+      for (const _middleware of handler) {
+        const middleware = _middleware as TMiddleware
+        try {
+          // note: only resolve when next is called, even if the middleware is a promise
+          await new Promise<void>((resolve, reject) => {
+            if (middleware.length !== 3) {
+              Promise.resolve(middleware(enrichedReq, res)).then(resolve).catch(reject)
+              return;
+            }
+            const maybePromise = middleware(enrichedReq, res as unknown as any, (err: any) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve()
+              }
+            })
+            if (maybePromise instanceof Promise) {
+              maybePromise
+                // .then(resolve) // do we need to resolve?
+                .catch(reject)
+            }
+          })
+          if (error) {
+            break handlerLoop
+          }
+        } catch (err) {
+          error = this.wrapError(err, error)
+          break handlerLoop
+        }
+      }
+      if (error) {
+        break handlerLoop
+      }
+    }
+
+    if (error) {
+      const errorHandlers = this.getErrorHandlers(methodLower, url)
+      for (const { pattern, handler } of errorHandlers) {
+        const paramsFromPattern = await this.extractParams(path, pattern, enrichedReq, res)
+        Object.assign(params, paramsFromPattern)
+        for (const _middleware of handler) {
+          const middleware = _middleware as EMiddleware
+          try {
+            // note: only resolve when next is called, even if the middleware is a promise
+            await new Promise<void>((resolve, reject) => {
+              const maybePromise = middleware(error, enrichedReq, res as unknown as any, (err: any) => {
+                if (err) {
+                  reject(err)
+                } else {
+                  resolve()
+                }
+              })
+
+              if (maybePromise instanceof Promise) {
+                maybePromise
+                  // .then(resolve) // do we need to resolve?
+                  .catch(reject)
+              }
+            })
+          } catch (err) {
+            error = this.wrapError(err, error)
+            break
+          }
+        }
+      }
+    }
+
+    if (!this.ensureEnded) return
+
+    if (res.writableEnded) return
+    if (error) {
+      console.debug('Error occurred but writes have not ended, sending 500 response', error)
+      res.statusCode = 500
+      res.end('Internal Server Error')
+    } else {
+      res.statusCode = 404
+      res.end('Not Found')
+    }
   }
 
   handle<TRequest extends { method?: string; url?: string } = IncomingMessage, TResponse extends { statusCode: number; end: (resp?: any) => void; writableEnded: boolean } = ServerResponse>(req: TRequest, res: TResponse): void {
@@ -434,8 +625,8 @@ export class RouterBase<TMiddleware extends Function = Middleware, EMiddleware e
 }
 
 export type RouterType<
-  TMiddleware extends Function = Middleware,
-  EMiddleware extends Function = ErrorMiddleware> =
+  TMiddleware extends TFunction = Middleware,
+  EMiddleware extends TFunction = ErrorMiddleware> =
   (() => RouterBase<TMiddleware, EMiddleware>) & {
     new(): RouterBase<TMiddleware, EMiddleware>
   }
